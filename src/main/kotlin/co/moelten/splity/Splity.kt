18e88511daac1ce5420c3dab0a007f1a8e9add36
Ryan Moelter <ryanmoelter@gmail.com>
2020-05-30 17:11:37 -0700

    Create project, naively generate ynab api

---
+++ src/main/kotlin/co/moelten/splity/Splity.kt
@@ -0,0 +1,13 @@
+package co.moelten.splity
+
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+  runBlocking {
+//    val budgetsApi = BudgetsApi
+//
+//    val something = budgetsApi.getBudgets(false)
+//
+//    println(something.data.budgets)
+  }
+}
