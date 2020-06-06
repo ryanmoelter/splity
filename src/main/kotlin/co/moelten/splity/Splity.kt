e315639b4d2a69072371e07c4925ff24fc206386
Ryan Moelter <ryanmoelter@gmail.com>
2020-06-06 00:18:28 -0700

    Add transaction + link (one-way)

--- src/main/kotlin/co/moelten/splity/Splity.kt
+++ src/main/kotlin/co/moelten/splity/Splity.kt
@@ -1,37 +1,125 @@
 package co.moelten.splity
 
-import com.github.scribejava.core.model.OAuth1AccessToken
 import com.youneedabudget.client.YnabClient
-import kotlinx.coroutines.coroutineScope
+import com.youneedabudget.client.models.Account
+import com.youneedabudget.client.models.BudgetSummary
+import com.youneedabudget.client.models.SaveTransaction
+import com.youneedabudget.client.models.SaveTransactionsWrapper
+import com.youneedabudget.client.models.TransactionDetail
 import kotlinx.coroutines.runBlocking
+import java.util.UUID
+
+// Copy these directly from YNAB, since some characters look similar but aren't (like apostrophes and dashes)
+const val RYANS_BUDGET_NAME = "2019.1"
+const val RYANS_SPLITWISE_ACCOUNT_NAME = "Split - Sarah"
+const val SARAHS_BUDGET_NAME = "Sarah’s Budget 2020"
+const val SARAHS_SPLITWISE_ACCOUNT_NAME = "Split - Ryan"
 
 fun main() {
   runBlocking {
     val ynab = YnabClient()
-    val splitwise = SplitwiseClient()
 
-    copySplitwiseIntoYnab(ynab, splitwise)
+    val budgetResponse = ynab.budgets.getBudgets(includeAccounts = true).data
+    val ryansBudget = budgetResponse.budgets.findByName(RYANS_BUDGET_NAME)
+    val ryansSplitwiseAccountId = ryansBudget.accounts!!.findByName(RYANS_SPLITWISE_ACCOUNT_NAME).id
+
+    val sarahsBudget = budgetResponse.budgets.findByName(SARAHS_BUDGET_NAME)
+    val sarahsSplitwiseAccountId = sarahsBudget.accounts!!.findByName(SARAHS_SPLITWISE_ACCOUNT_NAME).id
+
+    copyFromRyanToSarah(
+      ynab = ynab,
+      ryansAccountAndBudget = AccountAndBudget(ryansSplitwiseAccountId, ryansBudget.id),
+      sarahsAccountAndBudget = AccountAndBudget(sarahsSplitwiseAccountId, sarahsBudget.id)
+    )
+//    copyFromSarahToRyan(ynab)
   }
 }
 
-suspend fun copySplitwiseIntoYnab(ynab: YnabClient, splitwise: SplitwiseClient) = coroutineScope {
-  splitwise.groups
+suspend fun copyFromRyanToSarah(
+  ynab: YnabClient,
+  ryansAccountAndBudget: AccountAndBudget,
+  sarahsAccountAndBudget: AccountAndBudget
+) {
+  copyFromOneBudgetToAnother(
+    ynab = ynab,
+    fromAccountAndBudget = ryansAccountAndBudget,
+    toAccountAndBudget = sarahsAccountAndBudget
+  )
 }
 
-private suspend fun connectToSplitwise() {
-  val splitwise = SplitwiseClient()
-
-  println(splitwise.groups)
+suspend fun copyFromSarahToRyan(
+  ynabClient: YnabClient,
+  ryansAccountAndBudget: AccountAndBudget,
+  sarahsAccountAndBudget: AccountAndBudget
+) {
 }
 
-private fun setUpSplitwiseConnection() {
-  val splitwise = SplitwiseClient()
-  println(splitwise.authorizationUrl)
-  val verifier = readLine()!!
-  val token = splitwise.getAccessToken(verifier) as OAuth1AccessToken
-  println("""
-    token: ${token.token}
-    token secret: ${token.tokenSecret}
-    rawResponse: ${token.rawResponse}
-  """.trimIndent())
+suspend fun copyFromOneBudgetToAnother(
+  ynab: YnabClient,
+  fromAccountAndBudget: AccountAndBudget,
+  toAccountAndBudget: AccountAndBudget
+) {
+  val fromTransactions = ynab.transactions.getTransactionsByAccount(
+    budgetId = fromAccountAndBudget.budgetId.toString(),
+    accountId = fromAccountAndBudget.accountId.toString(),
+    sinceDate = null,
+    type = null,
+    lastKnowledgeOfServer = null
+  ).data.transactions
+    .map { it.id to it }
+    .toMap()
+  val toTransactions = ynab.transactions.getTransactionsByAccount(
+    budgetId = toAccountAndBudget.budgetId.toString(),
+    accountId = toAccountAndBudget.accountId.toString(),
+    sinceDate = null,
+    type = null,
+    lastKnowledgeOfServer = null
+  ).data.transactions
+    .map { it.id to it }
+    .toMap()
+
+  fromTransactions.values
+    .filter { transactionDetail ->
+      !(transactionDetail.payeeName?.startsWith("Starting Balance") ?: false)
+    }
+    .forEach { transactionDetail ->
+      if (!toTransactions.containsKey(transactionDetail.importId) && transactionDetail.approved) {
+        ynab.transactions.createTransaction(
+          toAccountAndBudget.budgetId.toString(),
+          SaveTransactionsWrapper(
+            SaveTransaction(
+              accountId = toAccountAndBudget.accountId,
+              date = transactionDetail.date,
+              amount = -transactionDetail.amount,
+              payeeId = null,
+              payeeName = transactionDetail.payeeName,
+              categoryId = null,
+              memo = transactionDetail.memo,
+              cleared = SaveTransaction.ClearedEnum.CLEARED,
+              approved = false,
+              flagColor = null,
+              importId = transactionDetail.id,
+              subtransactions = null
+            )
+          )
+        )
+      }
+    }
 }
+
+sealed class TransactionAction(open val transactionDetail: TransactionDetail) {
+  data class Create(override val transactionDetail: TransactionDetail) : TransactionAction(transactionDetail)
+  data class Approve(override val transactionDetail: TransactionDetail) : TransactionAction(transactionDetail)
+  data class Update(override val transactionDetail: TransactionDetail) : TransactionAction(transactionDetail)
+  data class Delete(override val transactionDetail: TransactionDetail) : TransactionAction(transactionDetail)
+  data class Replace(override val transactionDetail: TransactionDetail) : TransactionAction(transactionDetail)
+}
+
+data class TransactionsAndActions(val transactions: List<TransactionDetail>, val actions: List<TransactionAction>)
+data class AccountAndBudget(val accountId: UUID, val budgetId: UUID)
+
+fun List<BudgetSummary>.findByName(name: String) =
+  find { it.name == name } ?: throw IllegalStateException("Can't find budget: \"$name\"")
+
+fun List<Account>.findByName(name: String) =
+  find { it.name == name } ?: throw IllegalStateException("Can't find account: \"$name\"")
--- ynab-api/src/main/kotlin/com/youneedabudget/client/ApiBuilders.kt
+++ ynab-api/src/main/kotlin/com/youneedabudget/client/ApiBuilders.kt
@@ -4,11 +4,14 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.youneedabudget.client.apis.AccountsApi
 import com.youneedabudget.client.apis.BudgetsApi
+import com.youneedabudget.client.apis.TransactionsApi
 import com.youneedabudget.client.tools.GeneratedCodeConverters
 import com.youneedabudget.client.tools.TypesAdapterFactory
 import com.youneedabudget.client.tools.XNullableAdapterFactory
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level.BODY
 import retrofit2.Retrofit
 
 val moshi: Moshi = Moshi.Builder()
@@ -20,6 +23,9 @@ val moshi: Moshi = Moshi.Builder()
 
 
 val client: OkHttpClient = OkHttpClient.Builder()
+  .addInterceptor(HttpLoggingInterceptor().apply {
+    level = BODY
+  })
   .addInterceptor { chain ->
     val newRequest: Request = chain.request().newBuilder()
       .addHeader("Authorization", "Bearer $YNAB_API_KEY")
@@ -37,4 +43,5 @@ val retrofit: Retrofit = Retrofit.Builder()
 class YnabClient {
   val budgets: BudgetsApi by lazy { retrofit.create(BudgetsApi::class.java) }
   val accounts: AccountsApi by lazy { retrofit.create(AccountsApi::class.java) }
+  val transactions: TransactionsApi by lazy { retrofit.create(TransactionsApi::class.java) }
 }
