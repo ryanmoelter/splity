af99771c68ab89732a28e7798c376e460f27d9ed
Ryan Moelter <ryanmoelter@gmail.com>
2020-06-06 22:55:46 -0700

    Fully remove splitwise

--- splitwise-api/src/main/kotlin/com/splitwise/client/models/GroupsResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.splitwise.client.models
-
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = false)
-data class GroupsResponse(val groups: List<Group>)
-
-@JsonClass(generateAdapter = false)
-data class Group(val id: Int, val name: String)
